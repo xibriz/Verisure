fc191e9fc2f9b1655b0ef251830b3572de6c1205
Ruben Andreassen <rubean85@gmail.com>
2017-01-04 13:15:50 +0100

    Bugfix alarm status check

--- verisure.php
+++ verisure.php
@@ -6,17 +6,29 @@ require_once './lib/verisure.class.php';
 //Include Library Extension
 require_once './ext/iRobot.class.php';
 
+$debug = isset($_GET['debug']);
+
 $verisureLogonObj = new verisureLogon();
 if ($verisureLogonObj->runLogon()) {
+    if ($debug) {
+        echo 'Logon OK';
+    }
     sleep(2); //Wait a little
 } else {
+    if ($debug) {
+        echo 'Could not log in to the Verisure Portal';
+    }
     error_log("Could not log in to the Verisure Portal");
     exit;
 }
 
 $verisureRemoteControlObj = new verisureRemoteControl();
 //If alarm not of = alarm is on
-if (!$verisureRemoteControlObj->isAlarmOff()) {
+$alarmStatus = $verisureRemoteControlObj->isAlarmOff();
+if ($alarmStatus === false) {
+    if ($debug) {
+        echo 'Alarm is On.. Starting Roomba';
+    }
     //Start the iRobot Roomba
     $iRobotObj = new iRobot();
     $iRobotObj->start();
@@ -25,11 +37,18 @@ if (!$verisureRemoteControlObj->isAlarmOff()) {
     //TODO
 } 
 //Alarm is off
-else {
+else if ($alarmStatus === true) {
+    if ($debug) {
+        echo 'Alarm is Off.. Docking Roomba';
+    }
     //Dock the iRobot Roomba
     $iRobotObj = new iRobot();
     $iRobotObj->dock();
     
     //Turn the light on
     //TODO
+} else {
+    if ($debug) {
+        echo 'Unknown Alarm Status';
+    }
 }
